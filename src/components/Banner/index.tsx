99456f0c2ed1086e4c85a6ec32bd430843726bb1
cleyton vinicius <cleytonterceiro@outlook.com>
2022-12-03 17:22:54 -0300

    🚧 create a BannerSlider component and correction BannerProps

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -6,7 +6,7 @@ import * as S from './styles'
 export type BannerProps = {
   img: string
   title: string
-  Subtitle: string
+  subtitle: string
   buttonLabel: string
   buttonLink: string
   ribbon?: ReactNode
@@ -18,7 +18,7 @@ export const Banner = (props: BannerProps) => {
   const {
     img,
     title,
-    Subtitle,
+    subtitle,
     buttonLabel,
     buttonLink,
     ribbon,
@@ -35,7 +35,7 @@ export const Banner = (props: BannerProps) => {
       <S.Image src={img} role="img" aria-label={title} />
       <S.Caption>
         <S.Title>{title}</S.Title>
-        <S.Subtitle dangerouslySetInnerHTML={{ __html: Subtitle }} />
+        <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
 
         <Button as="a" href={buttonLink} size="large">
           {buttonLabel}
--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -7,7 +7,7 @@ export default {
   args: {
     img: 'https://images.unsplash.com/photo-1634132386045-2eef4dbda885?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80',
     title: 'Defy death',
-    Subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death'
   },
